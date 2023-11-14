2084f14c04bbb95e69bc4a14051baae7db625967
Sumit Yadav <sumit2911997@gmail.com>
2023-11-15 00:57:03 +0530

    initial project setup

---
+++ src/main/java/com/devworks/springdatajpapocs/SpringdatajpapocsApplication.java
@@ -0,0 +1,13 @@
+package com.devworks.springdatajpapocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpapocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpapocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devworks/springdatajpapocs/SpringdatajpapocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devworks.springdatajpapocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpapocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
